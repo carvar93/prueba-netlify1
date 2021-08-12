4e691f4d37f330acfff05ec89b3db5667dc373cd
Carlos Andrés <cavu93@hotmail.com>
2021-08-11 19:58:16 -0600

    commit v8 creo

--- Regna/mail.php
+++ Regna/mail.php
@@ -8,10 +8,10 @@ $priority = $_POST['priority'];
 $type = $_POST['type'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
-$recipient = "YOUREMAIL@HERE.COM";
+$recipient = "cavaal93@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . ;
 
 ?>
