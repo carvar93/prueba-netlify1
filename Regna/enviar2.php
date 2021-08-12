e0b1000d41ac5cdc02573a5f48865177af38b052
Carlos Andrés <cavu93@hotmail.com>
2021-08-11 18:14:54 -0600

    v4

--- Regna/enviar2.php
+++ Regna/enviar2.php
@@ -13,10 +13,10 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'ignaciobluuweb@gmail.com';
+$para = 'cavaal93@gmail.com';
 $asunto = 'Mensaje de mi sitio web';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
-header("Location:index.html");
+header("Location:https://zen-villani-e65918.netlify.app/?");
 ?>
