6d7fa257d6d7e4fe1812afac184cfa4ff17b8af4
Carlos Andrés <cavu93@hotmail.com>
2021-08-11 19:51:33 -0600

    commit v6 creo

--- Regna/enviar_email.php
+++ Regna/enviar_email.php
@@ -1,22 +1,21 @@
 <?php
-$nombre = $_POST['nombre'];
-$mail = $_POST['email'];
-$empresa = $_POST['mensaje'];
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+$emisor=$_POST['emisor'];
+$destinatario="cavaaal93@gmail.com";
+$mensaje=$_POST['mensaje'];
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+//Estoy recibiendo el formulario, compongo el cuerpo
+$cuerpo = "<h1>Un mensaje para ti</h1>";
+    
+$cuerpo .= "<p>Tu amigo " . $emisor . ", te envia este mensaje.</p>";
+$cuerpo .= "<p>Tu email: " . $destinatario . "</p>";
 
-$para = 'cavaal93@gmail.com';
-$asunto = 'Mensaje de mi sitio web';
+$cuerpo = $cuerpo.$mensaje;
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
+//mando el correo...
+mail($destinatario,"Mensaje de un amigo",$cuerpo,"MIME-Version: 1.0\nContent-type: text/html; charset=UTF-8\nFrom: ".$emisor." < http://www.miweb.com >");
 
-header("Location:https://zen-villani-e65918.netlify.app/?");
+//doy las gracias por el envio
+echo "<div style=\"background-color:green;color:white;padding:4px;text-align:center;\">Gracias por rellenar el formulario. Se ha enviado correctamente. Enviale a otros contactos</div>";
+
 ?>
